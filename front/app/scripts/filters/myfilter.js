c994bd02669eb7eef84b899a78d30f0217489de0
non-professinal <nomuyoshi0111@gmail.com>
2016-03-20 22:41:31 +0900

    frontをpublicに移動 grunt build -f

--- front/app/scripts/filters/myfilter.js
+++ front/app/scripts/filters/myfilter.js
@@ -10,10 +10,16 @@
  */
 angular.module('frontApp')
   .filter('truncate', function () {
-    return function (text, len = 130) {
+    return function (text, len) {
       if (!angular.isString(text)) {
         return text;
       }
+      // default値
+      if (len === undefined) {
+        // デフォルトは10文字
+        len = 130;
+      }
+
       if (text.length > len) {
         return text.substring(0, len-1) + "...";
       } else {
